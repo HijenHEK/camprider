ce900ea19ebae39445763efd223f87a2a9938804
HijenHek <k.hijen@gmail.com>
2021-03-21 18:58:06 +0100

    menu + responsive

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ window.addEventListener('load' ,()=>{
             nav.classList.add('scrolling')
         }else{
             nav.classList.remove('scrolling')
+            if(toggleMenu.classList.contains('menu-toggle') && scrollY == 50) {
+                toggleMenu.classList.remove('menu-toggle') ;
+            }  
         }
+    });
+    var toggleBtn = document.getElementById('toggler');
+    var toggleMenu = document.getElementById('toggle');
+    toggleBtn.addEventListener('click' , ()=>{
+        if(toggleMenu.classList.contains('menu-toggle')) {
+
+            toggleMenu.classList.remove('menu-toggle') ;
+            if(scrollY <= 50 ) nav.classList.remove('scrolling');
+            return;
+        }  
+        toggleMenu.classList.add('menu-toggle') ;
+        nav.classList.add('scrolling');
+
+
+        
     });
 });
